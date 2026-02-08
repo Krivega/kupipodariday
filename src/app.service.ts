65debd6ca729f03666e8c6668243dbabcecd4ae2
Krivega <mr.krivega@gmail.com>
2026-02-08 15:30:53 +0300

    chore: remove ESLint configuration and update dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly message = 'Hello World!';
+
+  public getHello(): string {
+    return this.message;
   }
 }
