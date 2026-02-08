23a7b4f5dafe447ed3819a4954bffc659be265f5
Krivega <mr.krivega@gmail.com>
2026-02-08 21:28:45 +0300

    feat: add unit tests for Offer, User, Wish, and Wishlist entities to validate schema and constraints

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -43,19 +43,19 @@ export class User {
   @MaxLength(200)
   about!: string; // информация о пользователе, строка от 2 до 200 символов. В качестве значения по умолчанию укажите для него строку: «Пока ничего не рассказал о себе».
 
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
+  avatar!: string; // ссылка на аватар. В качестве значения по умолчанию задайте https://i.pravatar.cc/300
+
   @Column({ unique: true })
   @IsNotEmpty()
   @IsEmail()
-  email!: string; // email, уникальная строка, обязательное поле
+  email!: string; // адрес электронной почты пользователя, должен быть уникален
 
   @Column()
   @IsString()
   @MinLength(6)
-  password!: string; // password, строка от 6 до 32 символов, обязательное поле
-
-  @Column({ default: 'https://i.pravatar.cc/300' })
-  @IsUrl()
-  avatar!: string; // сылка на аватар. В качестве значения по умолчанию задайте https://i.pravatar.cc/300
+  password!: string; // пароль пользователя, строка
 
   @OneToMany(
     () => {
