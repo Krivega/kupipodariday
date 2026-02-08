728ecfd1aafd5b5fdb67c88e474ec50e3ef10f09
Krivega <mr.krivega@gmail.com>
2026-02-08 15:39:52 +0300

    chore: update TypeScript configuration and improve error handling in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT ?? 3000);
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+
+// eslint-disable-next-line no-console
+bootstrap().catch(console.error);
