a1f27de8242e7af88eec459e86d337e53103fe7a
Krivega <mr.krivega@gmail.com>
2026-02-08 16:38:25 +0300

    feat: enhance User entity with validation and additional fields for username, about, and email

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,16 @@
 import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsEmail,
+  MaxLength,
+} from 'class-validator';
+import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
+  Column,
 } from 'typeorm';
 
 @Entity()
@@ -15,4 +23,27 @@ export class User {
 
   @UpdateDateColumn()
   updatedAt!: Date;
+
+  @Column({ unique: true })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(30)
+  username!: string;
+
+  @Column({ default: 'Пока ничего не рассказал о себе' })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(200)
+  about!: string;
+
+  @Column({ unique: true })
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @Column()
+  @IsString()
+  @MinLength(6)
+  password!: string;
 }
