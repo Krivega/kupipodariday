0fb8bf0f84c89185098135ceafa880bdb924104b
Krivega <mr.krivega@gmail.com>
2026-02-10 20:23:43 +0300

    feat: enable CORS for local development and update server port to 3001

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 
 // eslint-disable-next-line no-console
