2573afa9a9f6fd60c1e236b84446bbaf819f1f17
Krivega <mr.krivega@gmail.com>
2026-02-12 01:57:20 +0300

    refactor: update ForbiddenException messages in wishes and wishlists to provide clearer context on modification and deletion permissions

--- src/wishlists/exceptions.ts
+++ src/wishlists/exceptions.ts
@@ -3,4 +3,6 @@ import { NotFoundException, ForbiddenException } from '@nestjs/common';
 export const wishlistNotFoundException = new NotFoundException(
   'Wishlist not found',
 );
-export const wishlistForbiddenException = new ForbiddenException('');
+export const wishlistForbiddenException = new ForbiddenException(
+  'You can only modify or delete your own wishlists',
+);
